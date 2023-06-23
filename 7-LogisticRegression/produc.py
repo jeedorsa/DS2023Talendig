93d6b03b0e589b5b5c4d06677586f548b16feef5
Jesus Ortiz <jesus.ortiz@sansano.usm.cl>
2023-06-22 20:24:32 -0400

    Correccion

--- 7-LogisticRegression/produc.py
+++ 7-LogisticRegression/produc.py
@@ -1,13 +1,15 @@
 import pandas as pd
 import pickle
 import numpy as np
-model=pickle.load(open("/Users/vn55le3/Downloads/DS2023Talendig/7-LogisticRegression/modelo_logistico.sav",'rb'))
+model=pickle.load(open("/Users/vn55le3/Downloads/DS2023Talendig/7-LogisticRegression/modelo1.sav",'rb'))
+escalador=pickle.load(open("/Users/vn55le3/Downloads/DS2023Talendig/7-LogisticRegression/escalador.sav",'rb'))
 X=[]
 for i in range(4):
     cad="Digite la caracteristica"+str(i+1)
     d=float(input(cad))
     X.append(d)
 X=np.array(X)
-pred=model.predict(X.reshape(1,-1))
+X=escalador.transform(X.reshape(1,-1))
+pred=model.predict(X)
 print("La clase predicha es "+str(pred))
 
